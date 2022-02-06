e1371cbb6c807f61cf911da2e5e2d77e368d55f9
KaranIses <dominic.fietz@hotmail.de>
2022-02-06 21:20:34 +0100

    added new spider for degewo, added fix for possible Error 400

--- apartements/apartements/spiders/ebay_spider.py
+++ apartements/apartements/spiders/ebay_spider.py
@@ -6,7 +6,7 @@ class EbaySpider(scrapy.Spider):
     start_urls = [
         'https://www.ebay-kleinanzeigen.de/s-wohnung-mieten/berlin/anzeige:angebote/preis:100:600/c203l3331'
         '+wohnung_mieten.etage_i:1%2C8+wohnung_mieten.qm_d:35%2C60+wohnung_mieten.swap_s:nein+wohnung_mieten'
-        '.zimmer_d:1%2C2 '
+        '.zimmer_d:1%2C2'
     ]
 
     def parse(self, response):
@@ -15,5 +15,5 @@ class EbaySpider(scrapy.Spider):
                 'Adresse': titles.css("div.aditem-main--top--left::text").getall(),
                 'Titel': titles.css("a.ellipsis::text").getall(),
                 'Preis': titles.css("p.aditem-main--middle--price::text").getall(),
-                'Zusatz': titles.css("p.text-module-end span.simpletag.tag-small::text").getall(),
+                'Zusatz': titles.css("p.text-module-end span.simpletag.tag-small::text").getall()
             }
