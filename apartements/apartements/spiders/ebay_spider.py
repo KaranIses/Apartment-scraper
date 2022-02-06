096099190e784cf4a1104a92d3860a33086e9704
KaranIses <dominic.fietz@hotmail.de>
2022-02-06 20:16:34 +0100

    created new spider for ebay-kleinanzeigen, set listening to robot.txt to False

---
+++ apartements/apartements/spiders/ebay_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class EbaySpider(scrapy.Spider):
+    name = "ebay"
+    start_urls = [
+        'https://www.ebay-kleinanzeigen.de/s-wohnung-mieten/berlin/anzeige:angebote/preis:100:600/c203l3331'
+        '+wohnung_mieten.etage_i:1%2C8+wohnung_mieten.qm_d:35%2C60+wohnung_mieten.swap_s:nein+wohnung_mieten'
+        '.zimmer_d:1%2C2 '
+    ]
+
+    def parse(self, response):
+        for titles in response.css('article.aditem'):
+            yield {
+                'Adresse': titles.css("div.aditem-main--top--left::text").getall(),
+                'Titel': titles.css("a.ellipsis::text").getall(),
+                'Preis': titles.css("p.aditem-main--middle--price::text").getall(),
+                'Zusatz': titles.css("p.text-module-end span.simpletag.tag-small::text").getall(),
+            }
